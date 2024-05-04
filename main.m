033d031ba914dc2f629de96dd61e57f6be952140
Vaishnavi Shivkumar <vaishnavi.shivkumar@students.iiit.ac.in>
2024-05-05 02:23:01 +0530

    i think modulation is done check once

--- main.m
+++ main.m
@@ -33,21 +33,25 @@ a = 0.5;
 
 line_vector_rect = conv(symbols_vector_up,p1,'same');
 line_vector_rcos = conv(symbols_vector_up, p2, 'same');
-figure;
-plot(1:m*k,line_vector_rect);
-hold on
-plot(1:m*k, line_vector_rcos);
-hold on
-stem(1:m*k, symbols_vector_up);
-hold off
+% figure;
+% plot(1:m*k,line_vector_rect);
+% hold on
+% plot(1:m*k, line_vector_rcos);
+% hold on
+% stem(1:m*k, symbols_vector_up);
+% hold off
 %-------------------------------------------------------------------------------------
 %MODULATION PART: here there is no doppler effect so dsb-sc
 %rectangular encoded: 
 fc = 1e6;
 len_ip = length(symbols_vector_up);
-fs = 1e4;
-t = (0:(len_ip-1)) / fs;
+fs = 2e6;
+t = 0:1/fs:(len_ip-1/fs);
 carrier = cos(fc*2*pi*t);
+% ts = 1/fs;
+line_vector_rect = repelem(line_vector_rect, fs);
+line_vector_rcos = repelem(line_vector_rcos, fs);
+% disp(length(line_vector_rcos)); disp(length(line_vector_rect)); disp(length(carrier));
 modulated_rect_vec = line_vector_rect.*carrier;
 modulated_rcos_vec = line_vector_rcos .* carrier; 
 figure;
